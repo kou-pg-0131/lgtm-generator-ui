f298196a9d88390641e4c742b7b8e97c749b7552
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-21 08:02:15 +0900

    ApiClient を作成

---
+++ src/domain/lgtm.ts
@@ -0,0 +1,6 @@
+export type Lgtm = {
+  id: string;
+  status: string;
+  created_at: string;
+  url: string;
+};
---
+++ src/infrastructures/apiClient.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import * as path from 'path';
+import { Lgtm } from '../domain';
+
+export class ApiClient {
+  private endpoints = {
+    v1: {
+      lgtms: path.join(process.env.REACT_APP_API_ORIGIN, 'v1/lgtms'),
+    },
+  };
+
+  public async getLgtms(): Promise<{ lgtms: Lgtm[]; evaluated_id: string; }> {
+    const response = await axios.get(this.endpoints.v1.lgtms, { headers: { 'Content-Type': 'application/json' } });
+    return response.data;
+  }
+
+  public async createLgtm(params: { base64: string; }): Promise<Lgtm> {
+    const response = await axios.post(this.endpoints.v1.lgtms, JSON.stringify(params), { headers: { 'Content-Type': 'application/json' } });
+    return response.data;
+  }
+}
