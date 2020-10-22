874b22300f7bb87f3fbe860c52179abfbe5ad797
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-22 20:27:49 +0900

    Container を追加

--- src/views/layouts/main/index.tsx
+++ src/views/layouts/main/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 import { LgtmsPage, NotFoundPage } from '../../pages';
 
 export const Main: React.FC = () => {
   return (
-    <React.Fragment>
+    <Container maxWidth='lg'>
       <Switch>
         <Route exact path='/' component={LgtmsPage}/>
         <Route       path='*' component={NotFoundPage}/>
       </Switch>
-    </React.Fragment>
+    </Container>
   );
 };
