89597af8975ee56548aba88f0611649bc34e67b2
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-23 20:14:13 +0900

    ヘッダを装飾

--- src/views/layouts/header/index.tsx
+++ src/views/layouts/header/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { AppBar, Typography, Toolbar } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    logo: {
+      fontSize: 36,
+    },
+  }),
+);
 
 export const Header: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div>header</div>
+    <AppBar position='static' >
+      <Toolbar>
+        <Link to='/'>
+          <Typography variant='h1' className={classes.logo}>LGTM Generator</Typography>
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 };
