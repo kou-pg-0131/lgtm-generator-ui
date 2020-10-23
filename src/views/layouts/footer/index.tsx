e57af493d2b23bd8d022290e9343610719e7722d
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-23 20:28:38 +0900

    フッタを装飾

--- src/views/layouts/footer/index.tsx
+++ src/views/layouts/footer/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Container, Typography } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      padding: 40,
+      textAlign: 'center',
+    },
+  }),
+);
 
 export const Footer: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div>footer</div>
+    <Container className={classes.root}>
+      <Typography>&copy; 2020 koki sato</Typography>
+    </Container>
   );
 };
