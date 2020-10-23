ded0a502c5979384257e302482a9a89eaff40fdb
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-23 22:17:45 +0900

    dataUrl -> base64

--- src/infrastructures/imageFileLoader.ts
+++ src/infrastructures/imageFileLoader.ts
@@ -1,18 +1,22 @@
 export type ImageFile = {
   name: string;
   type: string
-  dataUrl: string;
+  base64: string;
 };
 
 export class ImageFileLoader {
   public load(file: File): Promise<ImageFile> {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
-      reader.onload = () => resolve({
-        name: file.name,
-        type: file.type,
-        dataUrl: reader.result as string,
-      });
+
+      reader.onload = () => {
+        const dataUrl = reader.result as string;
+        resolve({
+          name: file.name,
+          type: file.type,
+          base64: dataUrl.slice(dataUrl.indexOf(',') + 1),
+        });
+      };
       reader.onerror = (err) => reject(err);
       reader.readAsDataURL(file.slice());
     });
