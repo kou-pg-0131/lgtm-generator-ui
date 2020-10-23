701015725bb9a8ed8b517b51c09e446031da63d9
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-23 20:16:43 +0900

    上に間隔を追加

--- src/views/layouts/main/index.tsx
+++ src/views/layouts/main/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Container } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { LgtmsPage, NotFoundPage } from '../../pages';
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      paddingTop: 38,
+    },
+  }),
+);
+
 export const Main: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <Container maxWidth='lg'>
+    <Container maxWidth='lg' className={classes.root}>
       <Switch>
         <Route exact path='/' component={LgtmsPage}/>
         <Route       path='*' component={NotFoundPage}/>
