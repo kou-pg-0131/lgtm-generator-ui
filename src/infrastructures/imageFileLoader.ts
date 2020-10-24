284abbf6f46d0062f2129f7cfd85eb79d8c567b2
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-24 16:07:18 +0900

    画像をリサイズ

--- src/infrastructures/imageFileLoader.ts
+++ src/infrastructures/imageFileLoader.ts
@@ -1,3 +1,5 @@
+import { loadImage, createCanvas } from 'canvas';
+
 export type ImageFile = {
   name: string;
   type: string
@@ -5,20 +7,41 @@ export type ImageFile = {
 };
 
 export class ImageFileLoader {
-  public load(file: File): Promise<ImageFile> {
-    return new Promise((resolve, reject) => {
+  public async load(file: File): Promise<ImageFile> {
+    const dataUrl: string = await new Promise((resolve, reject) => {
       const reader = new FileReader();
-
       reader.onload = () => {
-        const dataUrl = reader.result as string;
-        resolve({
-          name: file.name,
-          type: file.type,
-          base64: dataUrl.slice(dataUrl.indexOf(',') + 1),
-        });
+        resolve(`data:${file.type};base64,${this.dataUrlToBase64(reader.result as string)}`);
       };
       reader.onerror = (err) => reject(err);
       reader.readAsDataURL(file.slice());
     });
+    const image = await loadImage(dataUrl);
+
+    const sideLength = 500;
+    const [distWidth, distHeight] = ((): [number, number] => {
+      if (image.width > image.height) {
+        return [sideLength, sideLength / image.width * image.height];
+      } else {
+        return [sideLength / image.height * image.width, sideLength];
+      }
+    })();
+    const canvas = createCanvas(distWidth, distHeight);
+    const context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0, image.width, image.height, 0, 0, distWidth, distHeight);
+
+    return {
+      name: file.name,
+      type: 'image/png',
+      base64: this.dataUrlToBase64(canvas.toDataURL('image/png')),
+    };
+  }
+
+  private dataUrlToBase64(dataUrl: string): string {
+    return dataUrl.slice(dataUrl.indexOf(',') + 1);
+  }
+
+  private base64ToDataUrl(base64: string, imageType: string): string {
+    return `data:${imageType};base64,${base64}`;
   }
 }
