fdb31e98c41933b794db5785c501acaac775308c
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-25 11:48:54 +0900

    タブを作成

--- src/views/layouts/main/index.tsx
+++ src/views/layouts/main/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Container } from '@material-ui/core';
+import { Container, Paper, Tab, Tabs } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { useHistory, useLocation } from 'react-router-dom';
 import { LgtmsPage, NotFoundPage } from '../../pages';
 
 const useStyles = makeStyles(() =>
@@ -15,8 +16,33 @@ const useStyles = makeStyles(() =>
 export const Main: React.FC = () => {
   const classes = useStyles();
 
+  const history = useHistory();
+  const location = useLocation();
+  const [path, setPath] = useState<string>(location.pathname);
+
+  const handleChange = (e: React.ChangeEvent<unknown>, value: string) => {
+    history.push(value);
+  };
+
+  useEffect(() => {
+    setPath(location.pathname);
+  }, [location.pathname]);
+
   return (
     <Container maxWidth='lg' className={classes.root}>
+      <Paper>
+        <Tabs
+          value={path}
+          indicatorColor='primary'
+          textColor='primary'
+          onChange={handleChange}
+        >
+          <Tab label='LGTM 画像' value='/'/>
+          <Tab label='画像検索' value='/search_images'/>
+          <Tab label='お気に入り' value='/favorites'/>
+        </Tabs>
+      </Paper>
+
       <Switch>
         <Route exact path='/' component={LgtmsPage}/>
         <Route       path='*' component={NotFoundPage}/>
