f52fce7050643effeb6ed61ba56bdf1c8bf4b4ee
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-25 10:56:39 +0900

    閉じるボタンを追加

--- src/views/app.tsx
+++ src/views/app.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, IconButton } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 import { SnackbarProvider } from 'notistack';
 import { Wrapper, Header, Main, Footer } from './layouts';
 
 const history = createBrowserHistory();
 
 export const App: React.FC = () => {
+  const notistackRef = React.createRef<SnackbarProvider>();
+
+  const handleClickDissmiss = (key: React.ReactText) => {
+    notistackRef.current?.closeSnackbar(key);
+  };
+
   return (
     <React.Fragment>
       <CssBaseline/>
       <SnackbarProvider
+        ref={notistackRef}
+        action={(key) => (
+          <IconButton size='small' onClick={() => handleClickDissmiss(key)}>
+            <Close fontSize='small'/>
+          </IconButton>
+        )}
         maxSnack={3}
         anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
         variant='success'
