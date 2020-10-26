407b1f3d32060695a96becfdd91d2ff015bf79de
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-27 08:49:31 +0900

    ロゴの文字サイズを調整

--- src/views/layouts/header/index.tsx
+++ src/views/layouts/header/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { AppBar, Typography, Toolbar } from '@material-ui/core';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     logo: {
       color: '#fff',
       fontFamily: 'ArchivoBlack',
-      fontSize: 36,
+      fontSize: 30,
+      [theme.breakpoints.down('sm')]: {
+        fontSize: 24,
+      },
     },
   }),
 );
@@ -17,7 +20,7 @@ export const Header: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <AppBar position='static' >
+    <AppBar position='static'>
       <Toolbar>
         <Link to='/'>
           <Typography variant='h1' className={classes.logo}>LGTM Generator</Typography>
