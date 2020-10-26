e9327a336931a0d5c20e0a5a4211908f41bb1714
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-26 20:59:31 +0900

    画像を表示するところまで

--- src/infrastructures/apiClient.ts
+++ src/infrastructures/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import * as path from 'path';
-import { Lgtm } from '../domain';
+import { Image, Lgtm } from '../domain';
 
 export class ApiClient {
   private endpoints = {
@@ -20,22 +20,53 @@ export class ApiClient {
     return response.data;
   }
 
-  public async searchImages(params: { q: string; }): Promise<string[]> {
+  public async searchImages(params: { q: string; }): Promise<Image[]> {
     return new Promise(resolve => {
       setTimeout(() => {
-        const links = [
-          'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg',
-          'https://upload.wikimedia.org/wikipedia/commons/b/b1/VAN_CAT.png',
-          'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*',
-          'https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg',
-          'https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg',
-          'https://upload.wikimedia.org/wikipedia/commons/0/0b/Cat_poster_1.jpg',
-          'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
-          'https://static.toiimg.com/thumb/msid-67586673,width-800,height-600,resizemode-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg',
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
-          'https://icatcare.org/app/uploads/2018/08/CKD.png',
+        const images = [
+          {
+            title: 'The Shots Your Cat Needs',
+            url: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg',
+          },
+          {
+            title: 'Odd-eyed cat - Wikipedia',
+            url: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/VAN_CAT.png',
+          },
+          {
+            title: 'The Shots Your Cat Needs',
+            url: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*',
+          },
+          {
+            title: 'Coronavirus: Cat owners fear pets will make them sick - BBC News',
+            url: 'https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg',
+          },
+          {
+            title: 'International Cat Care | The ultimate resource on feline health ...',
+            url: 'https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg',
+          },
+          {
+            title: 'Cat - Wikipedia',
+            url: 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Cat_poster_1.jpg',
+          },
+          {
+            title: 'Thinking of getting a cat? | International Cat Care',
+            url: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+          },
+          {
+            title: '5 things that scare and stress your cat - Times of India',
+            url: 'https://static.toiimg.com/thumb/msid-67586673,width-800,height-600,resizemode-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg',
+          },
+          {
+            title: 'cat - Wiktionary',
+            url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
+          },
+          {
+            title: 'Chronic Kidney Disease | International Cat Care',
+            url: 'https://icatcare.org/app/uploads/2018/08/CKD.png',
+          },
         ];
-        resolve(links);
+
+        resolve(images);
       }, 1000);
     });
   }
--- src/views/pages/searchImagesPage/index.tsx
+++ src/views/pages/searchImagesPage/index.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Button, Card, CardMedia, CardActionArea, CircularProgress, Grid, InputAdornment, TextField } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Search } from '@material-ui/icons';
+import { Form } from '../../components';
+import { Image } from '../../../domain';
+import { ApiClient } from '../../../infrastructures';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    media: {
+      backgroundSize: 'contain',
+      height: 140,
+    },
+  }),
+);
 
 export const SearchImagesPage: React.FC = () => {
+  const classes = useStyles();
+
+  const [images, setImages] = useState<Image[]>([]);
+  const [query, setQuery] = useState<string>('');
+  const [searching, setSearching] = useState<boolean>(false);
+
+  const apiClient = new ApiClient();
+
+  const handleChangeQuery = (e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.currentTarget.value);
+
+  const handleSearch = () => {
+    setSearching(true);
+    apiClient.searchImages({ q: query }).then(images => {
+      setImages(images);
+    }).finally(() => {
+      setSearching(false);
+    });
+  };
+
   return (
-    <div>search images</div>
+    <React.Fragment>
+      <Form onSubmit={handleSearch}>
+        <TextField
+          fullWidth
+          variant='outlined'
+          value={query}
+          onChange={handleChangeQuery}
+          placeholder='キーワード'
+          InputProps={{
+            startAdornment: <InputAdornment position='start'><Search/></InputAdornment>,
+          }}
+        />
+      </Form>
+      {searching ? (
+        <Box alignItems='center'><CircularProgress/></Box>
+      ) : (
+        <Grid container spacing={2}>
+          {images.map((image, i) => (
+            <Grid key={i} item xs={6} sm={3} md={2}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia className={classes.media} image={image.url}/>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </React.Fragment>
   );
 };
