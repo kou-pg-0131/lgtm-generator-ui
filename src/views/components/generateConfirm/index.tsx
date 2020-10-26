12faf1755daf842416a1db2e4a824b4086fb55ed
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-26 22:32:12 +0900

    ModalCard を使うようにした

--- src/views/components/generateConfirm/index.tsx
+++ src/views/components/generateConfirm/index.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import { Card, CardActions, CardMedia, Modal } from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { LoadableButton } from '..';
+import { CardActions, CardMedia } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { LoadableButton, ModalCard } from '..';
 import { Image } from '../../../domain';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
-    card: {
-      left: '50%',
-      outline: 0,
-      position: 'absolute',
-      top: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: '50%',
-      [theme.breakpoints.down('sm')]: {
-        width: '75%',
-      },
-      [theme.breakpoints.down('xs')]: {
-        width: '90%',
-      },
-    },
     media: {
       backgroundSize: 'contain',
       height: 300,
@@ -30,31 +16,29 @@ const useStyles = makeStyles((theme: Theme) =>
 type Props = {
   image: Image;
   open: boolean;
-  onGenerate: () => any;
+  onGenerate: () => void;
   processing: boolean;
-  onClose: () => any;
+  onClose: () => void;
 };
 
 export const GenerateConfirm: React.FC<Props> = (props: Props) => {
   const classes = useStyles();
 
   return (
-    <Modal open={props.open} onClose={props.onClose}>
-      <Card className={classes.card}>
-        <CardMedia className={classes.media} image={props.image.url}/>
-        <CardActions>
-          <LoadableButton
-            fullWidth
-            loading={props.processing}
-            disabled={props.processing}
-            color='primary'
-            variant='contained'
-            onClick={props.onGenerate}
-          >
-            LGTM 画像を生成する
-          </LoadableButton>
-        </CardActions>
-      </Card>
-    </Modal>
+    <ModalCard open={props.open} onClose={props.onClose}>
+      <CardMedia className={classes.media} image={props.image.url}/>
+      <CardActions>
+        <LoadableButton
+          fullWidth
+          loading={props.processing}
+          disabled={props.processing}
+          color='primary'
+          variant='contained'
+          onClick={props.onGenerate}
+        >
+          LGTM 画像を生成する
+        </LoadableButton>
+      </CardActions>
+    </ModalCard>
   );
 };
