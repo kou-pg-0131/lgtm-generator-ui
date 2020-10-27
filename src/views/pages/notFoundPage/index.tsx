d5d41541ef31aa2fcc29db8cf23c2d730ae7a780
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-27 20:12:32 +0900

    NotFoundPage をリファクタ

--- src/views/pages/notFoundPage/index.tsx
+++ src/views/pages/notFoundPage/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    text: {
+      fontSize: 16,
+      [theme.breakpoints.down('xs')]: {
+        fontSize: 13,
+      },
+    },
+  }),
+);
 
 export const NotFoundPage: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div>page not found</div>
+    <Typography className={classes.text} align='center'>お探しのページは見つかりませんでした</Typography>
   );
 };
