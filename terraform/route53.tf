a86ef4c367743da66e617b0dfeee67d44488c92f
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-31 23:07:47 +0900

    CloudFront Distribution を作成

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -15,3 +15,15 @@ resource aws_acm_certificate_validation main {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [aws_route53_record.certificate_validation.fqdn]
 }
+
+resource aws_route53_record main {
+  zone_id = data.aws_route53_zone.main.id
+  name    = local.domain
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.deployment.domain_name
+    zone_id                = aws_cloudfront_distribution.deployment.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
