d713074fe2cfcf82301d9ce6d905a4acd491aefa
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-10-31 23:24:59 +0900

    bucket_prefix -> bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource aws_s3_bucket deployment {
-  bucket_prefix = "${local.prefix}-deployment"
+  bucket        = "${local.prefix}-deployment"
   acl           = "private"
   force_destroy = true
   tags          = { Name = "${local.prefix}-deployment" }
