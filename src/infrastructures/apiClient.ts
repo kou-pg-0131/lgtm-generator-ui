bb589035b79c0ce9e799f0455050518d2670d81a
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-04 07:57:43 +0900

    constructor でクラスを受け取るようにした

--- src/infrastructures/apiClient.ts
+++ src/infrastructures/apiClient.ts
@@ -1,5 +1,5 @@
 import { Report, ReportType, Image, Lgtm } from '../domain';
-import { IHttpClient, HttpClient, IUrlBuilder, UrlBuilder, QueryStringParameters } from '.';
+import { IHttpClient, IUrlBuilder, QueryStringParameters } from '.';
 
 export interface IApiClient {
   // GET /v1/lgtms
@@ -13,32 +13,37 @@ export interface IApiClient {
 }
 
 export class ApiClient implements IApiClient {
-  private httpClient: IHttpClient = new HttpClient();
-  private urlBuilder: IUrlBuilder = new UrlBuilder();
+  constructor(
+    private config: {
+      apiOrigin: string,
+      httpClient: IHttpClient,
+      urlBuilder: IUrlBuilder,
+    },
+  ) {}
 
   public async getLgtms(evaluatedId?: string): Promise<{ lgtms: Lgtm[]; evaluated_id?: string; }> {
     const endpoint = this.buildEndpoint('/v1/lgtms', { evaluated_id: evaluatedId });
-    return await this.httpClient.get<{ lgtms: Lgtm[]; evaluated_id?: string }>(endpoint);
+    return await this.config.httpClient.get<{ lgtms: Lgtm[]; evaluated_id?: string }>(endpoint);
   }
 
   public async createLgtm(params: { base64?: string; url?: string; }): Promise<Lgtm> {
     const endpoint = this.buildEndpoint('/v1/lgtms');
     const body = JSON.stringify(params);
-    return await this.httpClient.post<Lgtm>(endpoint, body, { 'Content-Type': 'application/json' });
+    return await this.config.httpClient.post<Lgtm>(endpoint, body, { 'Content-Type': 'application/json' });
   }
 
   public async createReport(params: { type: ReportType; text: string; lgtm: Lgtm; }): Promise<Report> {
     const endpoint = this.buildEndpoint('/v1/reports');
     const body = JSON.stringify({ type: params.type, text: params.text, lgtm_id: params.lgtm.id });
-    return await this.httpClient.post(endpoint, body, { 'Content-Type': 'application/json' });
+    return await this.config.httpClient.post(endpoint, body, { 'Content-Type': 'application/json' });
   }
 
   public async searchImages(params: { q: string; }): Promise<Image[]> {
     const endpoint = this.buildEndpoint('/v1/images', { q: params.q });
-    return await this.httpClient.get(endpoint);
+    return await this.config.httpClient.get(endpoint);
   }
 
   private buildEndpoint(path: string, params?: QueryStringParameters): string {
-    return this.urlBuilder.build(this.urlBuilder.join(process.env.REACT_APP_API_ORIGIN, path), params);
+    return this.config.urlBuilder.build(this.config.urlBuilder.join(this.config.apiOrigin, path), params);
   }
 }
--- src/infrastructures/apiClientFactory.ts
+++ src/infrastructures/apiClientFactory.ts
@@ -1,7 +1,11 @@
-import { IApiClient, ApiClient } from '.';
+import { IApiClient, ApiClient, HttpClient, UrlBuilder } from '.';
 
 export class ApiClientFactory {
   public create(): IApiClient {
-    return new ApiClient();
+    return new ApiClient({
+      apiOrigin: process.env.REACT_APP_API_ORIGIN,
+      httpClient: new HttpClient(),
+      urlBuilder: new UrlBuilder(),
+    });
   }
 }
