57a80e568addf327173f062c5dc0287db26d4d6d
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-04 21:59:55 +0900

    ご利用上の注意へのリンクを追加

--- src/views/layouts/footer/index.tsx
+++ src/views/layouts/footer/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Typography } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -12,6 +13,12 @@ const useStyles = makeStyles(() =>
       textAlign: 'center',
       width: '100%',
     },
+    copyright: {
+      fontSize: 16,
+    },
+    link: {
+      fontSize: 12,
+    },
   }),
 );
 
@@ -20,7 +27,8 @@ export const Footer: React.FC = () => {
 
   return (
     <Box className={classes.root}>
-      <Typography>&copy; 2020 koki sato</Typography>
+      <Typography className={classes.copyright}>&copy; 2020 koki sato</Typography>
+      <Typography className={classes.link}><Link to='/precaution'>ご利用上の注意</Link></Typography>
     </Box>
   );
 };
