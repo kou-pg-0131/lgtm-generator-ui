6f46d185b6288db7e8c6d626104003b175a9643c
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-08 16:11:14 +0900

    modules を削除

--- src/views/app.tsx
+++ src/views/app.tsx
@@ -4,14 +4,11 @@ import { createBrowserHistory } from 'history';
 import { CssBaseline, ThemeProvider, IconButton } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { Close } from '@material-ui/icons';
-import { Provider } from 'react-redux';
 import { SnackbarProvider } from 'notistack';
-import { createStore } from './modules';
 import { Theme } from './theme';
 import { Wrapper, Header, Main, Footer } from './layouts';
 
 const history = createBrowserHistory();
-const store = createStore();
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -33,28 +30,26 @@ export const App: React.FC = () => {
   return (
     <ThemeProvider theme={Theme}>
       <CssBaseline/>
-      <Provider store={store}>
-        <SnackbarProvider
-          ref={notistackRef}
-          action={(key) => (
-            <IconButton size='small' onClick={() => handleClickDissmiss(key)}>
-              <Close fontSize='small' className={classes.icon}/>
-            </IconButton>
-          )}
-          maxSnack={3}
-          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-          variant='success'
-          autoHideDuration={3000}
-        >
-          <Router history={history}>
-            <Wrapper>
-              <Header/>
-              <Main/>
-              <Footer/>
-            </Wrapper>
-          </Router>
-        </SnackbarProvider>
-      </Provider>
+      <SnackbarProvider
+        ref={notistackRef}
+        action={(key) => (
+          <IconButton size='small' onClick={() => handleClickDissmiss(key)}>
+            <Close fontSize='small' className={classes.icon}/>
+          </IconButton>
+        )}
+        maxSnack={3}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        variant='success'
+        autoHideDuration={3000}
+      >
+        <Router history={history}>
+          <Wrapper>
+            <Header/>
+            <Main/>
+            <Footer/>
+          </Wrapper>
+        </Router>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 };
