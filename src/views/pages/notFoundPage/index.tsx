8308cea0407edf711e67a63d4b5a9356ed6292b1
kou-pg-0131 <kou.pg.0131@gmail.com>
2020-11-13 22:06:54 +0900

    不要な import を削除

--- src/views/pages/notFoundPage/index.tsx
+++ src/views/pages/notFoundPage/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { Typography } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { Title } from '../../components';
