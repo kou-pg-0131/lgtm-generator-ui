898c69451906749714942cfa8befe57cbffdba0f
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-03-28 21:15:29 +0900

    タブを url 指定できるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { Box } from '@material-ui/core';
 import { Layout } from '../layout';
 import { useLgtms } from '../contexts';
 import { LgtmsPanel, SerachImagesPanel, FavoritesPanel, Tabs, TabValue, LgtmList, LgtmListItem } from '../components';
 
 const Home: React.FC = () => {
-  const { lgtms } = useLgtms();
+  const router = useRouter();
+
+  const getTab = (): TabValue => {
+    return (['lgtms', 'search_images', 'favorites'] as TabValue[]).find((e) => e === router.query.tab) || 'lgtms';
+  };
   const [currentTab, setCurrentTab] = useState<TabValue>('lgtms');
+
+  const { lgtms } = useLgtms();
 
   const handleTabChange = (value: TabValue) => {
     setCurrentTab(value);
+    router.replace({ search: `?tab=${value}` });
   };
+
+  useEffect(() => {
+    setCurrentTab(getTab());
+  }, [router.query.tab]);
 
   return (
     <Layout>
