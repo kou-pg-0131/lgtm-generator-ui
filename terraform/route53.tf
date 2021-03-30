cbf239035daf463bafc97e819436d9fe7459e314
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-03-30 17:00:10 +0900

    バックエンドを vercel に移行

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -18,12 +18,8 @@ resource "aws_acm_certificate_validation" "main" {
 
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.main.id
-  name    = local.domain
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.deployment.domain_name
-    zone_id                = aws_cloudfront_distribution.deployment.hosted_zone_id
-    evaluate_target_health = false
-  }
+  name    = "lgtm-generator"
+  type    = "CNAME"
+  records = ["cname.vercel-dns.com"]
+  ttl     = 300
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,4 @@
-variable "stage" { type = string }
-
 locals {
-  sub_domain = var.stage == "prod" ? "" : "${var.stage}."
-  domain     = "${local.sub_domain}lgtm-generator.kou-pg.com"
-  product    = "lgtm-generator-ui"
-  prefix     = "${local.product}-${var.stage}"
+  domain = "lgtm-generator.kou-pg.com"
+  prefix = "lgtm-generator-ui"
 }
