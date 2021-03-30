8d92a0de087c5e6977b2e1e33458eae903e813f7
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-03-30 11:24:46 +0900

    Precaution ページを作成

---
+++ src/pages/precaution.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../layout';
+
+const Precaution: React.FC = () => {
+  return (
+    <Layout>
+      Precaution
+    </Layout>
+  );
+};
+
+export default Precaution;
